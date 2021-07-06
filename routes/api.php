01028b26aab1a1848e577caff6ff0a8cefe3fe93
AndreyZakhvatoshin <mastetandron@gmail.com>
2021-07-06 16:29:44 +0500

    add resource routes for tasks

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,9 @@
 |
  */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+use App\Http\Controllers\TaskController;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::apiResource('/tasks', TaskController::class);
