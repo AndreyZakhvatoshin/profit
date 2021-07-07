fcb69e09a1d90eb02f87bfaa87dcc04a53d38b99
AndreyZakhvatoshin <mastetandron@gmail.com>
2021-07-07 10:48:36 +0500

    Create APIFormRequest

---
+++ app/Http/Requests/APIFormRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class APIFormRequest extends FormRequest
+{
+    /**
+     * Формат вывода ошибок валидации для API
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response(
+                ['errors' =>
+                    ['Fields' => $validator->errors()]],
+                Response::HTTP_UNPROCESSABLE_ENTITY,
+            )
+        );
+    }
+
+}
