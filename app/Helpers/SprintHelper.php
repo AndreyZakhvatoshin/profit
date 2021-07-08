d4cb2b1f7cd7f6a411a624355f0071a8b95942a0
AndreyZakhvatoshin <mastetandron@gmail.com>
2021-07-08 13:36:08 +0500

    change method

--- app/Helpers/SprintHelper.php
+++ app/Helpers/SprintHelper.php
@@ -2,15 +2,42 @@
 
 namespace App\Helpers;
 
+use App\Models\Sprint;
+use App\Models\SprintTask;
+use App\Models\Task;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
 
 class SprintHelper
 {
-    public static function createSprintName(string $week): string
+    public static function createSprintData(string $week): array
     {
         $carbon = Carbon::now();
-        $converted = Str::substr($carbon->year, 2, 2);
-        return $converted . '-' . $week;
+        $yearSub = Str::substr($carbon->year, 2, 2);
+        $sprintName = $yearSub . '-' . $week;
+        return [
+            'name' => $sprintName,
+            'year' => (string) $carbon->year,
+            'week' => $week,
+        ];
+    }
+
+    public static function checkEstimateTask(int $id): bool
+    {
+        $tasks_id = SprintTask::select('task_id')->where('sprint_id', $id)->get();
+        $tasks = Task::whereIn('id', $tasks_id)->get();
+        $messages = [];
+        foreach ($tasks as $task) {
+            if (!$task->estimate) {
+                $messages[$task->id] = 'Время выполнения не указано';
+            }
+        }
+        return empty($messages);
+    }
+
+    public static function hasActiveSprint(): bool
+    {
+        $sprints = Sprint::where('status', 'active')->get();
+        return empty($sprints);
     }
 }
